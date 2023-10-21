0e6e3ec1c52c367851f9a9e590dd7b749d3b1da9
notpritam <notpritamsharma@gmail.com>
2023-10-21 08:06:51 +0530

    Unfold 2023

--- app/envelope/page.js
+++ app/envelope/page.js
@@ -1,5 +1,5 @@
 "use client";
-import envelopAbi from "../../lib/envelopeAbi"
+import envelopAbi from "../../lib/envelopeAbi";
 import {
   Table,
   TableBody,
@@ -12,7 +12,7 @@ import {
 import { Copy, Facebook, Mail } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { readContracts , readContract} from "@wagmi/core";
+import { readContracts, readContract } from "@wagmi/core";
 
 function page() {
   const searchParams = useSearchParams();
@@ -20,46 +20,46 @@ function page() {
   const [addressofGift, setAddressofGift] = useState();
   const addS = searchParams.get("search");
 
-const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState(0);
 
-const [greeting, setGreeting] = useState("Loading....")
+  const [greeting, setGreeting] = useState("Loading....");
 
-const [transactionList, setTransactionList] =useState()
+  const [transactionList, setTransactionList] = useState();
 
   useEffect(() => {
     setAddressofGift(addS);
-    if(addS != null){
+    if (addS != null) {
       getEnvelopeDetail(addS);
     }
   }, []);
 
-  const getEnvelopeDetail=async (addF)=>{
-    console.log(addF)
-    console.log(envelopAbi)
-    const config = {abi:envelopAbi, address: addF}
-  const allData = await readContracts({
-    contracts :[
-      {
-        ...config,
-        functionName: "view_balance"
-      },
-      {
-        ...config,
-        functionName: "greeting"
-      },
-      {
-        ...config,
-        functionName: "getTxn"
-      }
-    ]
-  });
+  const getEnvelopeDetail = async (addF) => {
+    console.log(addF);
+    console.log(envelopAbi);
+    const config = { abi: envelopAbi, address: addF };
+    const allData = await readContracts({
+      contracts: [
+        {
+          ...config,
+          functionName: "view_balance",
+        },
+        {
+          ...config,
+          functionName: "greeting",
+        },
+        {
+          ...config,
+          functionName: "getTxn",
+        },
+      ],
+    });
 
-  console.log(allData, "all data");
+    console.log(allData, "all data");
 
-  setTransactionList(allData[2].result);
-  setBalance(allData[0]);
-  setGreeting(allData[1]);
-  }
+    setTransactionList(allData[2].result);
+    setBalance(allData[0]);
+    setGreeting(allData[1]);
+  };
   return (
     <div className="flex flex-col lg:p-8">
       <span className=" text-[35px] lg:text-[50px] font-semibold">
@@ -74,7 +74,7 @@ const [transactionList, setTransactionList] =useState()
       </span>
 
       <span>
-{greeting.result} {balance.result}
+        {greeting.result} {balance.result}
       </span>
 
       <Table className="mt-[5rem]">
